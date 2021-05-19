3d33147d5f2f3eb936450a5fe1e31a562b4c8da8
Maciej Zygmanowski <sppmacd@pm.me>
2021-05-19 15:06:07 +0200

    logging: Don't warn if ANSI escape codes are not supported

--- ps/logging.py
+++ ps/logging.py
@@ -4,16 +4,12 @@ import sys
 import config as config
 
 class __internal__:
-    ansi_warned = False
     ansi_enabled = None
 
 def allow_ansi_escape_codes():
     if __internal__.ansi_enabled == None:
         supported = os.name == "posix"
         enabled = os.isatty(1) and not config.args.disable_formatting
-        if not supported and enabled and not __internal__.ansi_warned:
-            print("ANSI escape codes are not supported")
-            __internal__.ansi_warned = True
         __internal__.ansi_enabled = supported and enabled
     return __internal__.ansi_enabled
     
