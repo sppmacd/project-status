1d73ebc71fe6bcf5e3a8f6a480c0203b447c6c67
Maciej Zygmanowski <sppmacd@pm.me>
2021-05-20 14:35:34 +0200

    detector: Add file_size attribute to each guess

--- ps/detector.py
+++ ps/detector.py
@@ -38,6 +38,7 @@ class DetectorRegistry:
             if guess != None:
                 for one_guess in guess:
                     one_guess.guesser = name
+                    one_guess.attributes["file_size"] = os.path.getsize(file.path)
                 matching_guesses += guess
                 
         if len(matching_guesses) == 0:
