c6088936ac1a6b20e07a7a71947edae915099b8f
Maciej Zygmanowski <sppmacd@pm.me>
2021-05-21 11:24:54 +0200

    Everywhere: Improve formatting

--- ps/display.py
+++ ps/display.py
@@ -2,6 +2,7 @@ import config as config
 import math
 
 from .logging import *
+from .guessers import FileGuess, FileType
 
 colors = []
 
@@ -84,37 +85,47 @@ def directory_fancy_display(dir):
         return
         
     # TODO: Detect terminal size
-    display_size = 150
+    # TODO: Display other guesses as "Other" category
+    display_size = 100
     total_line_count = 0
     
     for guess in formats:
         lines_of_code = guess.lines_of_code()
-        format_display_size = (lines_of_code if lines_of_code != None else 0) * display_size / total_lines_of_code
+        lines_of_code = lines_of_code if lines_of_code != None else 0
+        format_display_size = int(lines_of_code * display_size / total_lines_of_code)
         guess.attributes["format_display_size"] = format_display_size
+
     print()
     
     for guess in formats:
         if guess.lines_of_code() != None:
-            print(guess.file_type.to_fancy_string() + " - " + sgr("1", str(guess.lines_of_code())) + " lines of code")
+            print(" •", guess.file_type.to_fancy_string() + " - " + sgr("1", str(guess.lines_of_code())) + " lines of code")
     print()
 
     for i in range(len(formats) + 1):
     
         if i < len(formats):
+            space_drawn = False
             for j in range(i + 1):
                 if int(formats[j].attributes["format_display_size"]) > 0:
+                    if not space_drawn:
+                        print("    ", end="")
+                        space_drawn = True
+
                     sgr_val = "38;2;" + str(HSV(colors[j % len(colors)], 0.5, 0.8).to_rgb())
                     if j == i:
-                        print(sgr(sgr_val, "┌─ ") + formats[j].file_type.to_fancy_string() + " - " + sgr("1", str(formats[j].lines_of_code())) + " lines of code")
+                        print(sgr(sgr_val, "┌─ ") + formats[j].file_type.to_fancy_string() + " - " + sgr("1", str(formats[j].lines_of_code() * 100 // total_lines_of_code)) + "%")
                     else:
                         print(sgr(sgr_val, "│") + (math.floor(formats[j].attributes["format_display_size"] - 1) * " "), end="")
                     
         else:
+            print("    ", end="")
             for j in range(i):
                 if int(formats[j].attributes["format_display_size"]) > 0:
                     sgr_val = "38;2;" + str(HSV(colors[j % len(colors)], 0.5, 0.8).to_rgb())
                     print(sgr(sgr_val, "│") + (math.floor(formats[j].attributes["format_display_size"] - 1) * " "), end="")
-            print()
+            
+            print("\n    ", end="")
             
             color = 0
             for guess in formats:
@@ -122,7 +133,7 @@ def directory_fancy_display(dir):
 
                 range_val = math.floor(format_display_size)
                 for i in range(range_val):
-                    v = 0.8 - ((((i / range_val) - 0.5) * 2)**2/2)/4
+                    v = ((((i / range_val) - 0.5) * 2)**2/2)/2 + 0.6
                     print(sgr("1;38;2;" + str(HSV(colors[color % len(colors)], 0.5, v).to_rgb()), "▀"), end="")
 
                 color += 1
