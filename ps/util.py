d33292f89ef6fbe61e0e6f76b21f359bda370ae6
Maciej Zygmanowski <sppmacd@pm.me>
2021-05-24 19:36:10 +0200

    util: Add function to spawn process in specified cwd and return stdout

--- ps/util.py
+++ ps/util.py
@@ -1,3 +1,14 @@
+import subprocess
+
+from .logging import print_error
+
+def run_process_in_dir_and_return_stdout(cwd, args):
+    try:
+        process = subprocess.run(args.split(" "), stdout=subprocess.PIPE, text=True, check=True, cwd=cwd)
+    except:
+        print_error("Failed to run: " + args)
+        return None
+    return process.stdout
 
 def depth_indent(depth):
     return "  "*depth;
